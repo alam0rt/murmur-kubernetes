c1342dde992dcba3ab2bd61dd6d6b389d505607f
alam0rt <samlockart@gmail.com>
2021-09-11 13:45:17 +1000

    add

--- apply.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete configmap murmur-config
-kubectl create configmap murmur-config --from-file=murmur.ini
-kubectl apply -f 05-murmur-pvc.yaml
-kubectl apply -f 10-murmur-deployment.yaml
-kubectl apply -f 20-murmur-svc.yaml
